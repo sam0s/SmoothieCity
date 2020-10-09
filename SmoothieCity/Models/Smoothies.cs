d16403d1464dc010face0f6b84ef566137831523
sam0s <samtubbiscool@gmail.com>
2020-10-09 14:04:50 -0400

    Identity works? :)

---
+++ SmoothieCity/Models/Smoothies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmoothieCity.Models
+{
+    public partial class Smoothies
+    {
+        public Smoothies()
+        {
+            
+        }
+
+        public int SmoothieId { get; set; }
+        public string SmoothieName { get; set; }
+        public int? SmoothieCalories { get; set; }
+        public double? SmoothiePrice { get; set; }
+        public string SmoothieImage { get; set; }
+
+    }
+}
